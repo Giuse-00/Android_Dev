2b4916658cd64cefc1ac4847fda4a2fd1b9d9493
Giuse-00 <118907635+Giuse-00@users.noreply.github.com>
2022-12-06 18:34:23 +0100

    Iacono esercizio encapsulation 01

---
+++ Es11_Dev/src/it/develhope/encapsulation/House.java
@@ -0,0 +1,16 @@
+package it.develhope.encapsulation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class House {
+
+    public int floorsNumber;
+    public String address;
+    public String[] residentNames;
+
+}
+
